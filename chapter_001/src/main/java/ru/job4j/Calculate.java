0650a948fc59f37765888f6b01692edb356248a7
SergeyVolko <rusobraz@mail.ru>
2017-11-16 22:20:45 +0400

    1.4. JUnit

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -8,12 +8,22 @@ package ru.job4j;
  * @since 15.11.2017
  */
 public class Calculate {
+	/**
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	*/
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
+	}
     /**
      * Main.
      * @param args - args.
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+		Calculate calc = new Calculate();
+        System.out.println(calc.echo("Sergey Volkov"));
     }
 }
 
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for test task.
+*
+* @author Sergey Volkov (rusobraz@mail.ru)
+* @version $Id$
+* @since 16.11.2017
+*/
+package ru.job4j;
