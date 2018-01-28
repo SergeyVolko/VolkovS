5bfbf6a1e648b7e7a26b38c0b7ca7384d7bdb3eb
SergeyVolko <rusobraz@mail.ru>
2018-01-28 15:12:00 +0400

    2. Создания Manifest для проекта Tracker [#398]

---
+++ Tracker/src/main/java/ru/job4j/models/Item.java
@@ -0,0 +1,39 @@
+package ru.job4j.models;
+
+public class Item {
+    private String id;
+    private String name;
+    private String description;
+    private long create;
+
+    public Item(String name, String description, long create) {
+        this.name = name;
+        this.description = description;
+        this.create = create;
+    }
+
+    public Item(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public long create() {
+        return this.create;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
