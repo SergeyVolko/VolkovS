f05f351d13eff7daeb124c3176f08b032e0fb6b2
SergeyVolko <rusobraz@mail.ru>
2018-03-21 21:29:05 +0400

    Кофемашина. [#34741]

--- chapter_002/src/main/java/ru/job4j/cofee/CofeMashine.java
+++ chapter_002/src/main/java/ru/job4j/cofee/CofeMashine.java
@@ -9,51 +9,28 @@ import java.util.Arrays;
 
 public class CofeMashine {
 
-    public int[] changes(int value, int price) throws WrongMoneyException {
+    public int[] changes(int value, int price) {
         int sdacha;
-        int sum;
         int pos = 0;
         int[] nominal = {10, 5, 2, 1};
-        int dlinaMass = 0;
-        if (value >= price && value > 0 && price >= 0) {
-            sdacha = value - price;
-            sum = sdacha;
-            for (int i = 0; i < nominal.length; i++) {
-                dlinaMass += sdacha / nominal[i];
-                sdacha %= nominal[i];
-                if (sdacha == 0) break;
+        sdacha = value - price;
+        int[] massSdacha = new int[sdacha / nominal[1] + 3];
+        for (int i = 0; i < nominal.length; i++) {
+            if (sdacha >= nominal[i]) {
+                massSdacha[pos] = nominal[i];
+                sdacha -= nominal[i];
+                pos++;
+                i--;
             }
-
-            int[] massSdacha = new int[dlinaMass];
-            for (int i = 0; i < massSdacha.length; i++) {
-                if (sum / nominal[pos] > 0) {
-                    massSdacha[i] = nominal[pos];
-                    sum -= nominal[pos];
-                } else {
-                    pos++;
-                    i--;
-                }
-            }
-            return massSdacha;
-        } else {
-            throw new WrongMoneyException("No money!");
         }
+        return Arrays.copyOf(massSdacha, pos);
+
     }
 
-    public void init(int value, int price) {
-
-        try {
-            System.out.println(Arrays.toString(this.changes(value, price)));
-        } catch (WrongMoneyException e) {
-            System.out.println("No money");
-        }
-
-    }
 
     public static void main(String[] args) {
         CofeMashine cofeMashine = new CofeMashine();
-        cofeMashine.init(-123, 345);
-        cofeMashine.init(63, 45);
-
+        int[] mass = cofeMashine.changes(50, 32);
+        System.out.println(Arrays.toString(mass));
     }
 }
