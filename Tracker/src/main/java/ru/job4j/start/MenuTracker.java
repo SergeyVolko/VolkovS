a1226789c8dd142460817ca55aed26e98e20a838
SergeyVolko <rusobraz@mail.ru>
2018-04-05 19:16:44 +0400

    2. Travis CI #31727

--- Tracker/src/main/java/ru/job4j/start/MenuTracker.java
+++ Tracker/src/main/java/ru/job4j/start/MenuTracker.java
@@ -44,12 +44,12 @@ public class MenuTracker {
     }
 
     public void showMenu() {
-        System.out.printf("//////////////////////////////////////////////////////////\n");
-        System.out.printf("Menu\n");
+        System.out.printf("//////////////////////////////////////////////////////////%s", System.lineSeparator());
+        System.out.printf("Menu%s", System.lineSeparator());
         for (UserAction userAction : this.userActionList) {
-            System.out.printf("%s\n", userAction.info());
+            System.out.printf("%s%s", userAction.info(), System.lineSeparator());
         }
-        System.out.printf("//////////////////////////////////////////////////////////\n");
+        System.out.printf("//////////////////////////////////////////////////////////%s", System.lineSeparator());
     }
 
     private static class CreatItem extends BaseAction {
@@ -60,12 +60,12 @@ public class MenuTracker {
 
         @Override
         public void execute(Input input, Tracker tracker) {
-            System.out.printf("------------ Adding new applications --------------\n");
+            System.out.printf("------------ Adding new applications --------------%s", System.lineSeparator());
             String name = input.ask("Enter the name of the application:");
             String desc = input.ask("Enter a description of the application :");
             Item item = new Item(name, desc);
             tracker.add(item);
-            System.out.printf("------------ A new application getId : %s -----------\n", item.getId());
+            System.out.printf("------------ A new application getId : %s -----------%s", item.getId(), System.lineSeparator());
         }
 
     }
@@ -78,14 +78,14 @@ public class MenuTracker {
 
         @Override
         public void execute(Input input, Tracker tracker) {
-            System.out.printf("------------Review of all applications---------------\n");
+            System.out.printf("------------Review of all applications---------------%s", System.lineSeparator());
             if (tracker.getAll().size() != 0) {
                 int index = 0;
                 for (Item item : tracker.getAll()) {
-                    System.out.printf("%s) id: %s name: %s description: %s\n", index++, item.getId(), item.getName(), item.getDescription());
+                    System.out.printf("%s) id: %s name: %s description: %s%s", index++, item.getId(), item.getName(), item.getDescription(), System.lineSeparator());
                 }
             } else {
-                System.out.printf("-----------You have no bids-------------\n");
+                System.out.printf("-----------You have no bids-------------%s", System.lineSeparator());
             }
         }
 
@@ -99,21 +99,21 @@ public class MenuTracker {
 
         @Override
         public void execute(Input input, Tracker tracker) {
-            System.out.printf("------------Edit the application id--------------\n");
+            System.out.printf("------------Edit the application id--------------%s", System.lineSeparator());
             String id = input.ask("Enter the id of the application you want to edit :");
             String name = input.ask("Enter the edited name of the application :");
             String desc = input.ask("Enter the edited description of the application :");
             Item item = new Item(name, desc);
             if (tracker.findById(id) != null) {
-                System.out.printf("------------- The parameters application id: %s ----------------\n", id);
-                System.out.printf("name: %s\n", tracker.findById(id).getName());
-                System.out.printf("description: %s\n", tracker.findById(id).getDescription());
+                System.out.printf("------------- The parameters application id: %s ----------------%s", id, System.lineSeparator());
+                System.out.printf("name: %s%s", tracker.findById(id).getName(), System.lineSeparator());
+                System.out.printf("description: %s%s", tracker.findById(id).getDescription(), System.lineSeparator());
                 System.out.println("Replaced with the following settings: ");
                 tracker.replace(id, item);
-                System.out.printf("new name: %s\n", tracker.findById(id).getName());
-                System.out.printf("new description: %s\n", tracker.findById(id).getDescription());
+                System.out.printf("new name: %s%s", tracker.findById(id).getName(), System.lineSeparator());
+                System.out.printf("new description: %s%s", tracker.findById(id).getDescription(), System.lineSeparator());
             } else {
-                System.out.printf("-----------Application id: %s does not exist-------------------\n", id);
+                System.out.printf("-----------Application id: %s does not exist-------------------%s", id, System.lineSeparator());
             }
         }
 
@@ -127,16 +127,16 @@ public class MenuTracker {
 
         @Override
         public void execute(Input input, Tracker tracker) {
-            System.out.printf("------------The withdrawal of an application by id --------------\n");
+            System.out.printf("------------The withdrawal of an application by id --------------%s", System.lineSeparator());
             String id = input.ask("Enter the id of the application you want to delete:");
             if (tracker.findById(id) != null) {
-                System.out.printf("Application id: %s\n", tracker.findById(id).getId());
-                System.out.printf("name: %s\n", tracker.findById(id).getName());
-                System.out.printf("description: %s\n", tracker.findById(id).getDescription());
+                System.out.printf("Application id: %s%s", tracker.findById(id).getId(), System.lineSeparator());
+                System.out.printf("name: %s%s", tracker.findById(id).getName(), System.lineSeparator());
+                System.out.printf("description: %s%s", tracker.findById(id).getDescription(), System.lineSeparator());
                 tracker.delete(id);
-                System.out.printf("-------------------Deleted----------------------\n");
+                System.out.printf("-------------------Deleted----------------------%s", System.lineSeparator());
             } else {
-                System.out.printf("----------- Application id: %s does not exist-------------------\n", id);
+                System.out.printf("----------- Application id: %s does not exist-------------------%s", id, System.lineSeparator());
             }
         }
 
@@ -150,15 +150,15 @@ public class MenuTracker {
 
         @Override
         public void execute(Input input, Tracker tracker) {
-            System.out.printf("---------------The output of the application to the screen id---------------\n");
+            System.out.printf("---------------The output of the application to the screen id---------------%s", System.lineSeparator());
             String id = input.ask("Enter the id of the application which you want to display:");
             Item item = tracker.findById(id);
             if (item != null) {
-                System.out.printf("------------The parameters application id: %s ------------------\n", id);
-                System.out.printf("name: %s\n", item.getName());
-                System.out.printf("description: %s\n", item.getDescription());
+                System.out.printf("------------The parameters application id: %s ------------------%s", id, System.lineSeparator());
+                System.out.printf("name: %s%s", item.getName(), System.lineSeparator());
+                System.out.printf("description: %s%s", item.getDescription(), System.lineSeparator());
             } else {
-                System.out.printf("-----------Application id: %s does not exist-------------------\n", id);
+                System.out.printf("-----------Application id: %s does not exist-------------------%s", id, System.lineSeparator());
             }
         }
     }
@@ -171,17 +171,17 @@ public class MenuTracker {
 
         @Override
         public void execute(Input input, Tracker tracker) {
-            System.out.printf("------------The choice of applications by name--------------\n");
+            System.out.printf("------------The choice of applications by name--------------%s", System.lineSeparator());
             String name = input.ask("Enter the name of the application / applications :");
             List<Item> items = tracker.findByName(name);
             int index = 0;
             if (items.size() != 0) {
-                System.out.printf("----------You have the following application name: %s ----------------\n", name);
+                System.out.printf("----------You have the following application name: %s ----------------%s", name, System.lineSeparator());
                 for (Item item : items) {
-                    System.out.printf("%s) id: %s description: %s\n", index++, item.getId(), item.getDescription());
+                    System.out.printf("%s) id: %s description: %s%s", index++, item.getId(), item.getDescription(), System.lineSeparator());
                 }
             } else {
-                System.out.printf("-------------Applications with the name: %s does not exist-------------------\n", name);
+                System.out.printf("-------------Applications with the name: %s does not exist-------------------%s", name, System.lineSeparator());
             }
         }
     }
@@ -193,7 +193,7 @@ public class MenuTracker {
 
         @Override
         public void execute(Input input, Tracker tracker) {
-            System.out.printf("------------Exit!--------------\n");
+            System.out.printf("------------Exit!--------------%s", System.lineSeparator());
         }
     }
 }
