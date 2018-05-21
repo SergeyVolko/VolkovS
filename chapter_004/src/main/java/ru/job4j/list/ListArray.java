7adc1a03a2653875623ee90d5c0b8489eb067c95
SergeyVolko <rusobraz@mail.ru>
2018-05-21 22:06:51 +0400

    5.3.1. Создать динамический список на базе массива. [#158]

---
+++ chapter_004/src/main/java/ru/job4j/list/ListArray.java
@@ -0,0 +1,66 @@
+package ru.job4j.list;
+/**
+ * @author Sergey Volkov (rusobraz@mail.ru)
+ * @version $Id$
+ * @since 22.05.2018
+ */
+
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ListArray<E> implements Iterable<E> {
+
+    private Object[] container;
+    private int countElement = 0;
+    private int modCount = 0;
+
+    public ListArray() {
+        this.container = new Object[10];
+    }
+
+    public void add(E value) {
+        if (this.countElement == this.container.length) {
+            this.container = Arrays.copyOf(this.container, this.container.length + 10);
+        }
+        this.container[this.countElement++] = value;
+        modCount++;
+    }
+
+    public E get(int index) {
+        return ((index >= 0) && (index < this.container.length)) ? (E) this.container[index] : null;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            int expectedModCount = modCount;
+            int indexArray = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return indexArray < countElement;
+            }
+
+            @Override
+            public E next() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (indexArray >= countElement) {
+                    throw new NoSuchElementException();
+                }
+                return (E) container[indexArray++];
+            }
+        };
+    }
+
+    @Override
+    public String toString() {
+        return "ListArray{" + "container=" + Arrays.toString(container) + '}';
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/iterator/listtest/ListArrayTest.java
@@ -0,0 +1,64 @@
+package ru.job4j.iterator.listtest;
+/**
+ * @author Sergey Volkov (rusobraz@mail.ru)
+ * @version $Id$
+ * @since 22.05.2018
+ */
+
+import org.junit.Test;
+import org.junit.Before;
+import ru.job4j.list.ListArray;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ListArrayTest {
+    private ListArray<Integer> listArray;
+
+    @Before
+    public void beforeTest() {
+        listArray = new ListArray<>();
+        for (int i = 0; i < 10; i++) {
+            listArray.add(i + 1);
+        }
+    }
+
+    @Test
+    public void whenAddElementToContainerThenAdding() {
+        this.beforeTest();
+        String expect = "ListArray{container=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}";
+        assertThat(expect, is(listArray.toString()));
+    }
+
+    @Test
+    public void whenNoMoreSize() {
+        this.beforeTest();
+        listArray.add(11);
+        String expect = "ListArray{container=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, null, null, null, null, null, null, null, null, null]}";
+        assertThat(expect, is(listArray.toString()));
+    }
+
+    @Test
+    public void whenGetThenElement() {
+        this.beforeTest();
+        int expect = 5;
+        assertThat(expect, is(listArray.get(4)));
+    }
+
+    @Test
+    public void whenCreateIteratorAndException() {
+        boolean flag = false;
+        this.beforeTest();
+        Iterator<Integer> iterator = listArray.iterator();
+        listArray.add(11);
+        try {
+            iterator.hasNext();
+        } catch (ConcurrentModificationException e) {
+            flag = true;
+        }
+        assertThat(true, is(flag));
+    }
+}
