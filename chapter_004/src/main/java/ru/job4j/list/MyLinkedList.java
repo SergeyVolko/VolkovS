a0acad00a2d6dd7f08af705a30a8d375300996ea
SergeyVolko <rusobraz@mail.ru>
2018-05-31 20:43:52 +0400

    5.3.2. Создать контейнер на базе связанного списка  [#159]

---
+++ chapter_004/src/main/java/ru/job4j/list/MyLinkedList.java
@@ -0,0 +1,75 @@
+package ru.job4j.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MyLinkedList<E> implements Iterable<E>{
+
+    private Node<E> first = new Node<>(null);
+    private Node<E> last;
+    private int size;
+    private int modCount;
+
+    public void add(E value) {
+        Node<E> result = new Node<>(value);
+        if (this.first.next == null) {
+            this.first.next = result;
+        } else {
+            this.last.next = result;
+        }
+        this.last = result;
+        this.size++;
+        this.modCount++;
+    }
+
+    public E get(int index) {
+        Node<E> result = this.first;
+        for (int i = 0; i < index; i++) {
+           result = result.next;
+        }
+        return result.value;
+
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            int expectedModCount = modCount;
+            Node<E> replace = first;
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return replace.next != null;
+            }
+
+            @Override
+            public E next() {
+
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (replace.next == null) {
+                    throw new NoSuchElementException();
+                }
+                replace = replace.next;
+                return replace.value;
+            }
+        };
+    }
+
+    private static class Node<E> {
+        E value;
+        Node<E> next;
+
+         Node(E value) {
+            this.value = value;
+        }
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/iterator/listtest/MyLinkedListTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.iterator.listtest;
+
+import org.junit.Before;
+import org.junit.Test;
+import ru.job4j.list.MyLinkedList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+public class MyLinkedListTest {
+    private MyLinkedList<Integer> myLinkedList;
+    @Before
+    public void beforeTest() {
+        myLinkedList = new MyLinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            myLinkedList.add(i+1);
+        }
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenAddThenAddingElementOfList() {
+        beforeTest();
+        Iterator<Integer> iterator = myLinkedList.iterator();
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(1));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(2));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(3));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(4));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(5));
+        assertThat(iterator.hasNext(), is(false));
+        assertThat(myLinkedList.get(3), is(3));
+        assertThat(myLinkedList.getSize(), is(5));
+        myLinkedList.add(6);
+        iterator.hasNext();
+    }
+}
