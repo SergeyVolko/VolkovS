963d29feb4fc736b813c7a40d96e028ed4379160
SergeyVolko <rusobraz@mail.ru>
2018-08-02 12:32:47 +0400

    Пинг-Понг.

--- Chapter_005/src/main/java/ru/job4j/pingpong/RectangleMove.java
+++ Chapter_005/src/main/java/ru/job4j/pingpong/RectangleMove.java
@@ -1,6 +1,7 @@
 package ru.job4j.pingpong;
 
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
 /**
  * @author Sergey Volkov (rusobraz@mail.ru)
@@ -9,35 +10,41 @@ import javafx.scene.shape.Rectangle;
  */
 public class RectangleMove implements Runnable {
     private final Rectangle rect;
+    private  Stage stage;
+    private boolean flag = false;
 
-    public RectangleMove(Rectangle rect) {
+    public RectangleMove(Rectangle rect, Stage stage) {
         this.rect = rect;
+        this.stage = stage;
     }
 
     @Override
     public void run() {
         int znakX = 1;
         int znakY = 1;
-        while (true) {
-            if (this.rect.getX() >= 300) {
-                znakX = -1;
+        while (!Thread.interrupted()) {
+            if (this.rect.getX()<=0 || this.rect.getX() >= 300) {
+                znakX *= -1;
             }
-            if (this.rect.getX() <= 0) {
-                znakX = 1;
-            }
-            if (this.rect.getY() >= 300) {
-                znakY = -1;
-            }
-            if (this.rect.getY() <= 0) {
-                znakY = 1;
+            if (this.rect.getY()<=0 || this.rect.getY() >= 300) {
+                znakY *= -1;
             }
             this.rect.setX(this.rect.getX() + znakX);
             this.rect.setY(this.rect.getY() + znakY);
+            stage.setOnCloseRequest(event -> this.isPressed());
             try {
                 Thread.sleep(50);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            if (this.flag) {
+                Thread.currentThread().interrupt();
+                this.isPressed();
+            }
         }
+    }
+
+    private void isPressed() {
+        this.flag = !this.flag;
     }
 }
