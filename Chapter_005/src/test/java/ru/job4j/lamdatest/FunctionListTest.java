c0b5fdc03e09c163f5d8bdd688c36446139700e3
SergeyVolko <rusobraz@mail.ru>
2018-10-10 13:10:17 +0400

    1. Подсчет функции в диапазоне.

--- Chapter_005/src/test/java/ru/job4j/lamdatest/FunctionListTest.java
+++ Chapter_005/src/test/java/ru/job4j/lamdatest/FunctionListTest.java
@@ -15,15 +15,20 @@ import static org.junit.Assert.assertThat;
  * @since 28.09.2018
  */
 public class FunctionListTest {
+    private FunctionList functionList = new FunctionList();
+
     @Test
-    public void whenDiapasonThenCorrectList() {
-        FunctionList functionList = new FunctionList();
-        List<Double> list = functionList.diapason(2, 5,
-                (n) -> (2 * n));
-        assertThat(list, is(Arrays.asList(4.0, 6.0, 8.0)));
-        list = functionList.diapason(1, 4, (n) -> Math.pow(n, 2.0) + 1);
-        assertThat(list, is(Arrays.asList(2.0, 5.0, 10.0)));
-        list = functionList.diapason(1, 4, (n) -> Math.log(n) + 1);
-        assertThat(list, is(Arrays.asList(Math.log(1) + 1, Math.log(2) + 1, Math.log(3) + 1)));
+    public void whenDiapasonThenLineFunction() {
+        assertThat(functionList.diapason(2, 5, (n) -> (2 * n)), is(Arrays.asList(4.0, 6.0, 8.0)));
+    }
+
+    @Test
+    public void whenDiapasonThenSquareFunction() {
+        assertThat(functionList.diapason(1, 4, (n) -> Math.pow(n, 2.0) + 1), is(Arrays.asList(2.0, 5.0, 10.0)));
+    }
+
+    @Test
+    public void whenDiapasonThenLogFunction() {
+        assertThat(functionList.diapason(1, 4, (n) -> Math.log(n) + 1), is(Arrays.asList(Math.log(1) + 1, Math.log(2) + 1, Math.log(3) + 1)));
     }
 }
